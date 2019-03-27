11594c79ddd1f9fc169c16c8eaefd4df6424a600
lonnen <chris.lonnen@gmail.com>
2019-03-26 18:41:39 -0700

    Handle unexpected requests
    This change adds a 404 file and refactors the handler so that it will 
    respond with 404 to any request except a GET for the root document.

--- src/main.rs
+++ src/main.rs
@@ -14,36 +14,25 @@ fn main() {
     }
 }
 
-
-/* HTTP Request
-Method Request-URI HTTP-Version CRLF
-headers CRLF
-message-body
-*/
-
-/* HTTP response
-HTTP-Version Status-Code Reason-Phrase CRLF
-headers CRLF
-message-body
-*/
-
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let mut file = File::open("hello.html").unwrap();
-
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let (code, status_line, filename) = if buffer.starts_with(get) {
+        ("200", "OK", "hello.html")
     } else {
-        // some other request type
-    }
+        ("404", "NOT FOUND", "404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 {} {}\r\n\r\n{}", code, status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
