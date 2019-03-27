47b432563d8b5aa788b58d99f6eebdcfcc11f811
lonnen <chris.lonnen@gmail.com>
2019-03-26 18:16:34 -0700

    A brief detour: file handling
    Following along with the rust lang book 1.30 tutorial, we take a diversion to implement static file serving.
    
    This change introduces a new static file in the project root. We pull `std::fs::file` into scope to read the file into a string. The `format!` macro formats the response to include this.
    
    This response will be sent regardless of the verb, resource, query-string, or any other details of the response. Next we'll get into validating the response.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
@@ -29,10 +30,14 @@ message-body
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut file = File::open("hello.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
