cd5ea2a3bcf51b4a65d98c62ee912877e986bbff
lonnen <chris.lonnen@gmail.com>
2019-03-27 00:08:52 -0700

    temp -- based on fahrenheit. cannot be run on stable.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![feature(futures_api, async_await, await_macro)]
+extern crate fahrenheit;
+extern crate futures;
 extern crate rand;
 
 use rand::random;
@@ -8,14 +11,23 @@ use std::net::TcpStream;
 use std::thread;
 use std::time;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    for stream in listener.incoming() {
-        handle_connection(stream.unwrap());
+use fahrenheit::AsyncTcpListener;
+use fahrenheit::AsyncTcpStream;
+use futures::io::AsyncReadExt;
+use futures::stream::StreamExt;
+use std::net::SocketAddr;
+
+async fn listen(addr: &str) {
+    let addr: SocketAddr = addr.parse().unwrap();
+    let listener = AsyncTcpListener::bind(addr).unwrap();
+    let mut incoming = listener.incoming();
+
+    while let Some(stream) = await!(incoming.next()) {
+        fahrenheit::spawn(process(stream));
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn process(mut stream: AsyncTcpStream) {
     stream.write(b"HTTP/1.1 200 OK\r\n").unwrap();
 
     loop {
@@ -27,4 +39,8 @@ fn handle_connection(mut stream: TcpStream) {
         stream.flush().unwrap();
         println!("response: {}", response);
     }
+}
+
+fn main() {
+    fahrenheit::run(listen("127.0.0.1:8080"));
 }
