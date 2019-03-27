13bc4b60d7b7f00925fc3f6b386c247e4d14ed50
lonnen <chris.lonnen@gmail.com>
2019-03-26 19:49:08 -0700

    Begin the bog
    This change starts the transformation from a general server into a 
    tarpit.
    
    This removes all code related to reading the stream. We don't really 
    care what was requested -- just want to bog down the response.
    
    To that end we also remove all files and templates used to respond with 
    specific resources, as well as pruning out `std::fs::File`.
    
    Instead we add an infinite loop that rapidly sends the same HTTP header 
    on repeat.
    
    This server responds until the Pipe Breaks. Firefox times this out at 
    around 500ms. Since there is no error handling, this panic crashes the 
    server.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,24 @@
-use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
     // note: we are doing nothing to handle potential failures here
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:2222").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
+        handle_connection(stream.unwrap());
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
+    stream.write(b"HTTP/1.1 200 OK\r\n").unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-
-    let (code, status_line, filename) = if buffer.starts_with(get) {
-        ("200", "OK", "hello.html")
-    } else {
-        ("404", "NOT FOUND", "404.html")
-    };
-
-    let mut file = File::open(filename).unwrap();
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents).unwrap();
-
-    let response = format!("HTTP/1.1 {} {}\r\n\r\n{}", code, status_line, contents);
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    loop {
+        let (header, value) = ('1', '2');
+        let response = format!("X-{}: {}\r\n", header, value);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        println!("response: {}", response);
+    }
 }
