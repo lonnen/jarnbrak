cc2d0c2fd51ff703542945df018d81c34af8534b
lonnen <chris.lonnen@gmail.com>
2019-03-26 23:18:20 -0700

    Pace the responses
    This change brings into scope:
        `std::thread` for sleep
        `std::time` for duration
    
    It introduces a 5 second sleep between headers. This now soft ties up a 
    client forever(or until a hard supervisory timeout).

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 extern crate rand;
 
+use rand::random;
+
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-
-use rand::random;
+use std::thread;
+use std::time;
 
 fn main() {
-    // note: we are doing nothing to handle potential failures here
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-
     for stream in listener.incoming() {
         handle_connection(stream.unwrap());
     }
@@ -19,10 +19,10 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write(b"HTTP/1.1 200 OK\r\n").unwrap();
 
     loop {
-        let response = format!("X-{}: {}\r\n",
-            random::<u8>(),
-            random::<u8>()
-        );
+        // pace is the trick
+        thread::sleep(time::Duration::from_secs(5));
+
+        let response = format!("X-{}: {}\r\n", random::<u8>(), random::<u8>());
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
         println!("response: {}", response);
