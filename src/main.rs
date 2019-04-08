d9b779a4b33a9a7d691eb44da22b01f721158877
lonnen <chris.lonnen@gmail.com>
2019-04-07 18:16:08 -0700

    added command line args
    this change uses structop and clap to take a command line argument for the port. these conveniently generate a useful help file and argument handlers. This also introduces a new default behavior whereby the binary will ask the OS to supply a port if none is provided by the user.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 extern crate rand;
 use rand::random;
 
+extern crate structopt;
+use structopt::StructOpt;
+
 extern crate jarnbrak;
 use jarnbrak::ThreadPool;
 
 use std::io::prelude::*;
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::SocketAddr;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
 use std::time;
 
+#[derive(StructOpt)]
+struct Opt {
+    /// Port 0 (default) will request that the OS assigns a port.
+    #[structopt(short = "p", long = "port", default_value = "0")]
+    port: u16,
+}
+
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let opt = Opt::from_args();
+
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), opt.port);
+
+    let listener = TcpListener::bind(addr).unwrap();
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming().take(2) {
